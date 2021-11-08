eb78a04b098b721f23581ee984964a04f90084af
andrei <git1@email.com>
2021-11-08 19:51:40 +0100

    Fetching Data from Server in CourseDetails Activity

--- app/src/main/java/com/example/onlinecourseandroidapp/CourseDetails.java
+++ app/src/main/java/com/example/onlinecourseandroidapp/CourseDetails.java
@@ -4,11 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.onlinecourseandroidapp.model.CourseDetail;
+import com.example.onlinecourseandroidapp.retrofit.ApiInterface;
+import com.example.onlinecourseandroidapp.retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CourseDetails extends AppCompatActivity {
 
+    ApiInterface apiInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+
+
+        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+
+        Call<List<CourseDetail>> call = apiInterface.getAllLesson();
+
+        call.enqueue(new Callback<List<CourseDetail>>() {
+            @Override
+            public void onResponse(Call<List<CourseDetail>> call, Response<List<CourseDetail>> response) {
+
+                List<CourseDetail> courseDataList = response.body();
+
+            }
+
+
+
+            @Override
+            public void onFailure(Call<List<CourseDetail>> call, Throwable t) {
+
+            }
+        });
+
+
     }
 }
